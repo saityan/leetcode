0d9cac941d29528352b3bc0c68981bcecddab5f9
saityan <naminatosky@gmail.com>
2023-03-29 17:39:56 +0300

    Add files via upload

---
+++ 1402_ReducingDishes.kt
@@ -0,0 +1,24 @@
+package leetcode
+
+private class ReducingDishes {
+    fun maxSatisfaction(satisfaction: IntArray): Int {
+        if (satisfaction.isEmpty())
+            return 0
+
+        val dishes = IntArray(satisfaction.size)
+        satisfaction.copyInto(dishes)
+        dishes.sort()
+
+        var currentSum = 0
+        var sum = 0
+        var index = dishes.lastIndex
+
+        while (index >= 0 && dishes[index] + currentSum > 0) {
+            currentSum += dishes[index]
+            sum += currentSum
+            index--
+            println(currentSum.toString() + "\t" + sum.toString())
+        }
+        return sum
+    }
+}
