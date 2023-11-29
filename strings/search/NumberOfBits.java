11a3a97a4cc24d07a73f4e1575f25845f3939aa3
saityan <naminatosky@gmail.com>
2023-11-29 13:17:47 +0300

    Number of 1 Bits

---
+++ strings/search/NumberOfBits.java
@@ -0,0 +1,16 @@
+package leetcode.strings.search;
+
+/**
+ * leetcode.com/problems/number-of-1-bits/
+ */
+
+public class NumberOfBits {
+    public int hammingWeight(int n) {
+        int result = 0;
+        while (n != 0) {
+            result += n & 1;
+            n >>>= 1;
+        }
+        return result;
+    }
+}
