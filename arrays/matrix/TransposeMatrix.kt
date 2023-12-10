aa932ed16bbeb44eca5ebfae1bdeb422a470593c
saityan <naminatosky@gmail.com>
2023-12-10 20:43:52 +0300

    Transpose Matrix

---
+++ arrays/matrix/TransposeMatrix.kt
@@ -0,0 +1,14 @@
+package leetcode.arrays.matrix
+
+/**
+ * https://leetcode.com/problems/transpose-matrix/
+ */
+
+private class TransposeMatrix {
+    fun transpose(matrix: Array<IntArray>): Array<IntArray> =
+        matrix[0].indices.map { i ->
+            matrix.map {
+                it[i]
+            }.toIntArray()
+        }.toTypedArray()
+}
