cac1ca5752b2356dad59ccde7a97dde8b9e8de96
saityan <naminatosky@gmail.com>
2023-12-13 14:12:33 +0300

    Special Positions in a Binary Matrix

---
+++ arrays/matrix/SpecialPositionsTotal.kt
@@ -0,0 +1,30 @@
+package leetcode.arrays.matrix
+
+/**
+ * https://leetcode.com/problems/special-positions-in-a-binary-matrix/
+ */
+
+private class SpecialPositionsTotal {
+    fun numSpecial(mat: Array<IntArray>): Int {
+        if (mat.isEmpty() || mat[0].isEmpty())
+            return 0
+
+        val rowSums = IntArray(mat.size)
+        val colSums = IntArray(mat[0].size)
+
+        for (i in mat.indices)
+            for (j in mat[0].indices) {
+                rowSums[i] += mat[i][j]
+                colSums[j] += mat[i][j]
+            }
+
+        var result = 0
+
+        for (i in mat.indices)
+            for (j in mat[0].indices)
+                if (mat[i][j] == 1 && rowSums[i] == 1 && colSums[j] == 1)
+                    result++
+
+        return result
+    }
+}
