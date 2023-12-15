2cf1f4544db0677557cfce45ed71a12aed3f75c3
saityan <naminatosky@gmail.com>
2023-12-15 12:33:46 +0300

    added "private" to the class

--- graphs/search/DestinationCity.kt
+++ graphs/search/DestinationCity.kt
@@ -4,7 +4,7 @@ package leetcode.graphs.search
  * https://leetcode.com/problems/destination-city/
  */
 
-class DestinationCity {
+private class DestinationCity {
     fun destCity(paths: List<List<String>>): String =
         paths.associate { it[0] to it[1] }.let { map ->
             paths.firstOrNull { !map.containsKey(it[1]) }?.get(1) ?: ""
