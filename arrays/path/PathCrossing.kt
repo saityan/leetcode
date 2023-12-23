9fc5b1fd1f7a413d29c03bd5233cf9156556a314
saityan <naminatosky@gmail.com>
2023-12-23 12:00:09 +0300

    Path Crossing

---
+++ arrays/path/PathCrossing.kt
@@ -0,0 +1,31 @@
+package leetcode.arrays.path
+
+/**
+ *
+ */
+
+private class PathCrossing {
+
+    fun isPathCrossing(path: String): Boolean {
+
+        if(path.length < 2) return false
+
+        val visited = mutableSetOf<Pair<Int, Int>>()
+        visited.add(Pair(0, 0))
+
+        var x = 0
+        var y = 0
+
+        for (c in path) {
+            when(c) {
+                'E' -> x++
+                'W' -> x--
+                'N' -> y++
+                'S' -> y--
+            }
+            if (!visited.add(Pair(x, y))) return true
+        }
+
+        return false
+    }
+}
