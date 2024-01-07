285f56ca317da688163b5de2904440286ca62e7f
saityan <naminatosky@gmail.com>
2024-01-07 17:06:30 +0300

    Maximum Product of Three Numbers

---
+++ arrays/search/MaximumProductOfThree.kt
@@ -0,0 +1,17 @@
+package leetcode.arrays.search
+
+/**
+ * https://leetcode.com/problems/maximum-product-of-three-numbers/
+ */
+
+class MaximumProductOfThree {
+    fun maximumProduct(nums: IntArray): Int {
+        if (nums.size < 3) return 0
+
+        val sortedNums = nums.sorted()
+
+        return maxOf(
+            sortedNums[0] * sortedNums[1] * sortedNums.last(),
+            sortedNums.takeLast(3).reduce { acc, v -> acc * v })
+    }
+}
