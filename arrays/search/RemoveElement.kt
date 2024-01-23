2193cf09bb733ee32ef4dd4e2ba93f9250190c0a
saityan <naminatosky@gmail.com>
2024-01-23 15:04:53 +0300

    Remove Element

---
+++ arrays/search/RemoveElement.kt
@@ -0,0 +1,18 @@
+package leetcode.arrays.search
+
+private class RemoveElement {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        if (nums.isEmpty()) return 0
+
+        var k = 0
+
+        for (i in nums.indices) {
+            if (nums[i] != `val`) {
+                nums[k] = nums[i]
+                k++
+            }
+        }
+
+        return k
+    }
+}
