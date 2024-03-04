a0fa9600e389942c77deeecf4c161e32dba0d1bc
saityan <naminatosky@gmail.com>
2024-03-04 11:22:24 +0300

    Merge Two Sorted Lists

---
+++ lists/MergeTwoSorted.kt
@@ -0,0 +1,20 @@
+package leetcode.lists
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists
+ */
+
+private class MergeTwoSorted {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        if (list1 == null) return list2
+        if (list2 == null) return list1
+
+        return if (list1.`val` < list2.`val`) {
+            list1.next = mergeTwoLists(list1.next, list2)
+            list1
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next)
+            list2
+        }
+    }
+}
