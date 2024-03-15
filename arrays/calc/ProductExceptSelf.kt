9202b4b75328f8cb0acbe33bd42de3471c25602b
saityan <naminatosky@gmail.com>
2024-03-15 12:43:23 +0300

    Product of Array Except Self

---
+++ arrays/calc/ProductExceptSelf.kt
@@ -0,0 +1,29 @@
+package leetcode.arrays.calc
+
+/**
+ * https://leetcode.com/problems/product-of-array-except-self/
+ */
+
+private class ProductExceptSelf {
+    fun productExceptSelf(nums: IntArray): IntArray {
+
+        if (nums.isEmpty()) return IntArray(0)
+
+        val size = nums.size
+        var leftSide = 1
+        var rightSide = 1
+        val result = IntArray(size) { 1 }
+
+        for (i in nums.indices) {
+            result[i] *= leftSide
+            leftSide *= nums[i]
+        }
+
+        for (i in nums.indices.reversed()) {
+            result[i] *= rightSide
+            rightSide *= nums[i]
+        }
+
+        return result
+    }
+}
